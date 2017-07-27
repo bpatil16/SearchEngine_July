5d84a781ffe893e9712397f2b24b84f878be701d
bhagyashree Patil <bhagyashreepatil@hotmail.com>
2017-07-27 14:43:54 -0700

    Fisrt code july 27

---
+++ SearchEngine/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    public enum AccountTypes
+    {
+        Jobseeker,
+        Employer
+    }
+
+    public class Account
+    {
+        #region Variables
+
+        private static int lastAccountNumber = 0;
+
+        #endregion
+
+        #region Properties
+
+            public int AccountNumber { get; private set;}
+            public string EmailAddress { get; set; }
+            public string Password { get; set; }
+            public AccountTypes TypeOfAccount { get; set; }
+
+
+        #endregion
+
+        #region Constructor
+
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+
+        #endregion
+    }
+}
---
+++ SearchEngine/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine
+{
+    public static class Engine
+    {
+        public static Account CreateAccount(string emailAddress, string password, AccountTypes typeofAccount)
+        {
+            var account = new Account
+            {
+                EmailAddress = emailAddress,
+                TypeOfAccount = typeofAccount
+            };
+            return account;
+        }
+    }
+}
