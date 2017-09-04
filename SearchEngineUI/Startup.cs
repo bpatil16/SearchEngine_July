ca84767dfa192598bc4bae809977b17af8b4bcb1
bhagyashree Patil <bhagyashreepatil@hotmail.com>
2017-09-04 00:32:39 -0700

    MVC Kickoff First time

---
+++ SearchEngineUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SearchEngineUI.Startup))]
+namespace SearchEngineUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
